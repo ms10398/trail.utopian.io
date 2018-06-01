7c934c0c0ba231bc1591dad9b5ad0d9943af643b
Mohit Sharma <msvbs98@gmail.com>
2018-06-01 20:23:36 +0530

    Example Config and Readme added

---
+++ README.md
@@ -0,0 +1,14 @@
+### Utopian Trail Bot
+
+For running it
+
+```
+npm install
+node app.js
+```
+
+For running on server
+
+```
+forever start app.js
+```
---
+++ config.example.js
@@ -0,0 +1,9 @@
+const config = {
+  username: '<Language Understanding API username here>',
+  password: '<Language Understanding API password here>',
+  url : 'https://gateway.watsonplatform.net/natural-language-understanding/api',
+  accname: '<Account to use for trailing here>',
+  posting: '<Posting key of the account>'
+}
+
+module.exports = config
