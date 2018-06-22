d4b03c5c0401ea577ef1589cc72f5c3c61194386
Mohit Sharma <msvbs98@gmail.com>
2018-06-22 08:57:22 +0530

    Increase confidence score

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ function StreamVote(author, permalink, weight, comment, check_context) {
                                         for(c = 0; c < response.categories.length; c++) {
                                             const category = response.categories[c];
 
-                                            if (labels.includes(category.label) === true && category.score >= 0.55) {
+                                            if (labels.includes(category.label) === true && category.score >= 0.65) {
                                                 vote = true;
                                             }
                                         }
